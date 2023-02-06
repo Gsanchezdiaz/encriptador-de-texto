ba7b0fa76f69d2fdd56f782085e9009d435b4557
Gerson Sanchez <gsanchezdiaz@outlook.com>
2023-02-06 13:03:41 -0500

    eliminando lineas vacias

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 
-
 function encriptar(stringEncriptada){
     let matrizKeys = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
-
     for(let i=0; i < matrizKeys.length; i++){
         if(stringEncriptada.includes(matrizKeys[i][0])){
             stringEncriptada = stringEncriptada.replaceAll(matrizKeys[i][0],matrizKeys[i][1]);
@@ -13,10 +11,10 @@ function encriptar(stringEncriptada){
     }
     return stringEncriptada;
 }
+
 function desencriptar(stringDesencriptada){
     let matrizKeys = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
-
     for(let i=0; i < matrizKeys.length; i++){
         if(stringDesencriptada.includes(matrizKeys[i][1])){
             stringDesencriptada = stringDesencriptada.replaceAll(matrizKeys[i][1],matrizKeys[i][0]);
@@ -40,8 +38,8 @@ function btnDesencriptar(){
 }
 
 function btnCopiar(){
-    const button = document.querySelector(".btn-copiar");
-    navigator.clipboard.writeText(mensaje.value);
-    button.textContent="Copiado";
-    mensaje.value = "";
+        const button = document.querySelector(".btn-copiar");
+        navigator.clipboard.writeText(mensaje.value);
+        button.textContent="Copiado";
+        mensaje.value = "";
 }
